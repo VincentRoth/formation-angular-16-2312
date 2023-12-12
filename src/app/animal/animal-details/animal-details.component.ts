fde161a13f865e2239b817b82e71de9573fa3bc6
Vincent Roth <vincent@placeme.io>
2023-12-12 17:05:40 +0100

    Routage initial

--- src/app/animal/animal-details/animal-details.component.ts
+++ src/app/animal/animal-details/animal-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Animal } from '../../shared/api/animal';
 import { AnimalService } from '../../shared/api/animal.service';
 import { FormatPhonePipe } from '../../shared/format-phone.pipe';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-animal-details',
@@ -13,10 +15,18 @@ export class AnimalDetailsComponent implements OnInit {
 
   constructor(
     private animalService: AnimalService,
-    private formatPhonePipe: FormatPhonePipe
+    private formatPhonePipe: FormatPhonePipe,
+    private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.animalService.get(1).subscribe((data) => (this.animal = data));
+    this.activatedRoute.paramMap
+      .pipe(
+        map((params: ParamMap): number => Number(params.get('id'))),
+        switchMap(
+          (id: number): Observable<Animal> => this.animalService.get(id)
+        )
+      )
+      .subscribe((data) => (this.animal = data));
   }
 }
--- src/app/animal/animal.module.ts
+++ src/app/animal/animal.module.ts
@@ -4,10 +4,15 @@ import { AnimalDetailsComponent } from './animal-details/animal-details.componen
 import { AnimalListComponent } from './animal-list/animal-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { AnimalItemComponent } from './animal-item/animal-item.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AnimalDetailsComponent, AnimalListComponent, AnimalItemComponent],
-  imports: [CommonModule, SharedModule],
-  exports: [AnimalDetailsComponent, AnimalListComponent],
+  declarations: [
+    AnimalDetailsComponent,
+    AnimalListComponent,
+    AnimalItemComponent,
+  ],
+  imports: [CommonModule, SharedModule, RouterModule],
+  exports: [],
 })
 export class AnimalModule {}
