3aecf9fe55852656d3d9697e0f592611710b0d8a
Vincent Roth <vincent@placeme.io>
2023-12-14 15:02:56 +0100

    Angular Material

--- src/app/animal/animal.module.ts
+++ src/app/animal/animal.module.ts
@@ -8,6 +8,11 @@ import { AnimalComponent } from './animal.component';
 import { AnimalRoutingModule } from './animal-routing.module';
 import { AnimalFormComponent } from './animal-form/animal-form.component';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,17 @@ import { FormsModule } from '@angular/forms';
     AnimalComponent,
     AnimalFormComponent,
   ],
-  imports: [CommonModule, SharedModule, AnimalRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AnimalRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   exports: [],
 })
 export class AnimalModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnimalModule } from './animal/animal.module';
 import { TimeInterceptor } from './shared/time.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AnimalModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AnimalModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TimeInterceptor, multi: true },
   ],
